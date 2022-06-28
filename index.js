2287ff2990f08a8484c746aaae1e8e3cd4ad033b
hung_nguyendang <hung.nguyendang@messagemedia.com>
2022-06-28 13:31:24 +0700

    config helmet

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const path = require('path')
 
 const app = express()
 
-app.use(helmet.frameguard({ action: 'DENY' }))
+app.use(helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+        defaultSrc: helmet.contentSecurityPolicy.dangerouslyDisableDefaultSrc,
+        frameAncestors: ["'none'"],
+    }
+}))
 app.use(express.static(path.join(__dirname, '/build')))
 
 const PORT = process.env.PORT
