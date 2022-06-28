7f1acab49afb1b4a539f5d8e15165db170cc1c5f
hung_nguyendang <hung.nguyendang@messagemedia.com>
2022-06-28 11:47:32 +0700

    config helmet

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const helmet = require('helmet')
 const path = require('path')
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, '/build')))
+app.use(helmet.frameguard({ action: 'SAMEORIGIN' }))
 
 const PORT = process.env.PORT
 
